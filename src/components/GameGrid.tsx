ec53a1e440fa8cd9dac1a06149d2a5692234246c
Abdul Majeed <dev.abdul.majeed@gmail.com>
2025-02-03 03:14:01 +0000

    Created a generic data fetching hook

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -4,7 +4,7 @@ import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 
 const GameGrid = () => {
-  const { games, error, isLoading } = useGames();
+  const { data: games, error, isLoading } = useGames();
   const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   return (
     <>
